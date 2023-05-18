5f4c1f66e6af642aa7606c3b45a38a522e633437
Salim Karim <imsjk8@gmail.com>
2023-05-18 21:15:13 +1000

    updated payload

--- helius-mongo.js
+++ helius-mongo.js
@@ -10,19 +10,18 @@ const server = http.createServer((req, res) => {
         req.on('data', (chunk) => {
             body.push(chunk);
         }).on('end', () => {
-            body = JSON.parse(Buffer.concat(body).toString());
+            // body = JSON.parse(Buffer.concat(body).toString());
 
             // transform the payload to suit the Discord webhook format
             const payload = {
-                body
-                // source: body?.body[0]?.source,
-                // description: body?.body[0]?.description,
-                // signature: body?.body[0]?.signature,
-                // buyer: body?.body[0]?.events.nft.buyer,
-                // seller: body?.body[0]?.events.nft.seller,
-                // price: body?.body[0]?.events.nft.price,
-                // type: body?.body[0]?.events.nft.type,
-                // nft: body?.body[0]?.events.nft.nfts[0].mint
+                source: body?.body[0]?.source,
+                description: body?.body[0]?.description,
+                signature: body?.body[0]?.signature,
+                buyer: body?.body[0]?.events.nft.buyer,
+                seller: body?.body[0]?.events.nft.seller,
+                price: body?.body[0]?.events.nft.price,
+                type: body?.body[0]?.events.nft.type,
+                nft: body?.body[0]?.events.nft.nfts[0].mint
             };
 
             // save to MongoDB
